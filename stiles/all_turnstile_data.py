e665609cceb0ac6f449bc8a86e524ade54c27452
Tiffany Moeller <tmoellerart@gmail.com>
2018-01-17 20:10:58 -0600

    getting and cleaning data notebook

--- stiles/all_turnstile_data.py
+++ stiles/all_turnstile_data.py
@@ -9,13 +9,13 @@ start17 = datetime(2017, 7, 1)
 start16 = datetime(2016, 7, 2)
 
 
-def get_week_list(startdate):
+def datelist(startdate):
     week_list = [startdate + ((timedelta(days=-7))*i) for i in range(14)]
     clean_weeks = [i.strftime('%y%m%d') for i in week_list]
     return clean_weeks
 
 
-weeks_to_import = get_week_list(start17) + get_week_list(start16)
+weeks_to_import = datelist(start17) + datelist(start16)
 
 
 def loadturndata(date):
